b44461ee5c077fff9e9c10df717471dfbe3f2b59
Booki Park <bkbkgg@gmail.com>
2023-02-06 20:59:46 +0900

    주문 취소 서비스 코드 작성

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -47,4 +49,25 @@ public class OrderService {
         return order.getId();
 
     }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancel(Long orderId) {
+
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+
+    }
+
+    /**
+     * 검색
+     */
+//    public List<Order> searchOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
 }
