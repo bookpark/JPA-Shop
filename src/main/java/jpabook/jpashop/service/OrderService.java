080b860955a6ceea4cf20b63d549730d6be5b434
Booki Park <bkbkgg@gmail.com>
2023-02-07 14:37:35 +0900

    method 명 변경

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -54,7 +54,7 @@ public class OrderService {
      * 주문 취소
      */
     @Transactional
-    public void cancel(Long orderId) {
+    public void cancelOrder(Long orderId) {
 
         // 주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
